57c775f9116ffbf7c0290333ca88a72cde3753df
alleninwood <alleninwood@gmail.com>
2018-11-04 01:42:05 -0800

    create project entity and corresponding repository :star:

---
+++ WoodPPMTool/src/main/java/io/woodintelligence/woodtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.woodintelligence.woodtool.repositories;
+
+import io.woodintelligence.woodtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
