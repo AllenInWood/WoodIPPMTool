ee11c8b0b86109e92b2d3d76de98d7c7da2a72ee
alleninwood <alleninwood@gmail.com>
2018-11-05 22:17:06 -0800

    refact error validation part

--- WoodPPMTool/src/main/java/io/woodintelligence/woodtool/web/ProjectController.java
+++ WoodPPMTool/src/main/java/io/woodintelligence/woodtool/web/ProjectController.java
@@ -1,21 +1,17 @@
 package io.woodintelligence.woodtool.web;
 
 import io.woodintelligence.woodtool.domain.Project;
+import io.woodintelligence.woodtool.services.MapValidationErrorService;
 import io.woodintelligence.woodtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -24,16 +20,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
-        if (result.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-
-            for (FieldError error : result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if (errorMap != null) {
+            return errorMap;
         }
 
         Project project1 = projectService.saveOrUpdateProject(project);
