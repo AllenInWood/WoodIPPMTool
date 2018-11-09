ff320e538b26deec9f1499fe530ab64ba6cd66d1
alleninwood <alleninwood@gmail.com>
2018-11-09 01:41:01 -0800

    import router

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,14 +3,20 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Dashboard />
+          <Route exact path="/dashboard" componenet={Dashboard} />
+          <Route exact path="/addProject" componenet={AddProject} />
+        </div>
+      </Router>
     );
   }
 }
